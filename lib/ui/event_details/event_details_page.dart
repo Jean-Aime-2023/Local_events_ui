c51a0d89957114f3e7b1903a1c17fa607bdd7847
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-01-21 18:44:32 +0200

    events content completed

--- lib/ui/event_details/event_details_page.dart
+++ lib/ui/event_details/event_details_page.dart
@@ -22,7 +22,7 @@ class EventDetailsPage extends StatelessWidget {
           fit: StackFit.expand,
           children: [
             EventDetailBackground(),
-            EventDetailContent()
+            EvlentDetailsContent()
           ],
         ),
       )
